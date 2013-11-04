481685c040b1ce1621069f7bc4311f5b4f5137f7
Luis Cielak <luis@luiscielak.com>
2013-11-04 18:32:48 -0500

    reorganzied ui styleguide site

--- emto-web/js/app.js
+++ emto-web/js/app.js
@@ -1,28 +1,14 @@
 // Custom Application JS
 
 
-// function handleSliderChange(event, slider){
-//   $('#vardiv').css('width', slider.value + '%');
-//   $("#vardiv").text(slider.value + '%');
+
+// function changeColor(){
+//   document.getElementById("input").style.backgroundColor = '#000000';
 // }
 
-
-// Handle Slider
-// $('#slider').slider({
-//   value: 7,
-//   slide: handleSliderChange
-// });w
-
-function changeColor(){
-  document.getElementById("input").style.backgroundColor = '#000000';
-}
-
-
+// Update values based on slider
 function updateSlider(sliderValue){
-  // var sliderValue = this.sliderValue;
-
   console.log(sliderValue);
   document.getElementById("ui-slider-value").innerHTML = sliderValue;
-
 }
 
