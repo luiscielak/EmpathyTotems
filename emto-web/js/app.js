8d3caf53b139578466c031b88341aad73d0bdad7
Luis Cielak <luis@luiscielak.com>
2013-11-04 12:27:53 -0500

    add app js file

---
+++ emto-web/js/app.js
@@ -0,0 +1,14 @@
+// Custom Application JS
+
+
+// function handleSliderChange(event, slider){
+//   $('#vardiv').css('width', slider.value + '%');
+//   $("#vardiv").text(slider.value + '%');
+// }
+
+
+// Handle Slider
+// $('#slider').slider({
+//   value: 7,
+//   slide: handleSliderChange
+// });w
